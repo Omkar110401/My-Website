fde7ca3e4f0756b2e8f00b9b7ba7f74859048b68
Omkar110401 <78097888+Omkar110401@users.noreply.github.com>
2023-09-09 00:49:36 +0530

    commit upto intro-section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { IntroComponent } from './intro/intro.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutMeComponent,
+    NotFoundComponent,
+    ContactMeComponent,
+    IntroComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/timeline/timeline-data.ts
@@ -0,0 +1,12 @@
+export const timelineData = [
+    {
+      year: 1934,
+      description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium"
+    },
+    {
+      year: 1937,
+      description: "Proin quam velit, efficitur vel neque vitae, rhoncus commodo mi. Suspendisse finibus mauris et bibendum molestie. Aenean ex augue, varius et pulvinar in, pretium non nisi."
+    },
+    // Add more timeline items here
+  ];
+  
